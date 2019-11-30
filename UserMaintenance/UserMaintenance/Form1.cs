a6e299e0faed9a0948b3807dda51f0444903f938
Renáta Keresztény <renata.kereszteny@stud.uni-corvinus.hu>
2019-11-30 13:50:51 +0100

    full

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -18,8 +18,9 @@ namespace UserMaintenance
 
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            //label1.Text = Resource1.LastName;
+            //label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -34,8 +35,9 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                //LastName = textBox1.Text,
+                // FirstName = textBox2.Text
+                FullName = textBox1.Text
             };
             users.Add(u);
         }
